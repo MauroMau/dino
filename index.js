57a369b56c6a62bc80d20dfbfed0356f6b26b448
MauroMau <148816479+MauroMau@users.noreply.github.com>
2024-09-08 13:50:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// extract from chromium source code by @liuwayong
+// extract from chromium source code by @mauromau
 (function () {
     'use strict';
     /**
-     * T-Rex runner.
+     * .
      * @param {string} outerContainerId Outer containing element id.
      * @param {Object} opt_config
      * @constructor
